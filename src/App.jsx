46eb4c80e739e957e1429e4d93536d29d13eeec1
victormusembi11 <victor.musembi11@gmail.com>
2024-05-12 01:40:54 +0300

    feat: Add toggle functionality to Example2 component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 // import Example1 from "./examples/example1";
 import Example2 from "./examples/example2";
 
 function App() {
+  const [toggle, setToggle] = useState(true);
   return (
     <div>
       {/* <Example1 /> */}
-      <Example2 />
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      {toggle && <Example2 />}
     </div>
   );
 }
