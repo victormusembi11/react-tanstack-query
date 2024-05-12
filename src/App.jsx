1c81a16898bd2579e649820f7f3af0afcb01a6fd
victormusembi11 <victor.musembi11@gmail.com>
2024-05-12 03:58:06 +0300

    feat: Add PostForm and PostsList components for managing posts

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from "react";
 
 // import Example1 from "./examples/example1";
 import Example2 from "./examples/example2";
+import { PostsProvider } from "./contexts/PostsContext";
+import PostsList from "./components/PostsList";
+import PostForm from "./components/PostForm";
 
-function App() {
-  const [toggle, setToggle] = useState(true);
+function App({ queryClient }) {
+  const [toggle, setToggle] = useState(false);
   return (
     <div>
       {/* <Example1 /> */}
       <button onClick={() => setToggle(!toggle)}>Toggle</button>
       {toggle && <Example2 />}
+      <PostsProvider>
+        <PostForm queryClient={queryClient} />
+        <PostsList />
+      </PostsProvider>
     </div>
   );
 }
