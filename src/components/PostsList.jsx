39e761d3c3056e916c0d5a72b3879924a0e7b106
victormusembi11 <victor.musembi11@gmail.com>
2024-05-13 11:11:43 +0300

    ref: Update PostsList component to use fetchPosts function from API module

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,42 +1,67 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
+import {
+  Flex,
+  Heading,
+  Alert,
+  AlertIcon,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
 
 import { PostsContext } from "../contexts/PostsContext";
 import { fetchPosts } from "../api/posts";
+import { POSTS_ACTIONS } from "../reducers/PostsReducer";
 
 export default function PostsList() {
-  const { state } = useContext(PostsContext);
+  const { state, dispatch } = useContext(PostsContext);
   const {
     data: posts,
     isLoading,
     isError,
+    error,
   } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
   });
+
+  useEffect(() => {
+    if (posts) {
+      dispatch({ type: POSTS_ACTIONS.SET_POSTS, payload: posts });
+    }
+  }, [posts, dispatch]);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   if (isError) {
-    return <div>Error</div>;
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        {error.message}
+      </Alert>
+    );
   }
 
   return (
-    <>
-      <h1>Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-      <h1>Posts from context</h1>
-      <ul>
-        {state.map((post, index) => (
-          <li key={index}>{post.title}</li>
-        ))}
-      </ul>
-    </>
+    <Flex m="2em">
+      <Flex flexDirection="column">
+        <Heading>Posts from API</Heading>
+        <UnorderedList>
+          {posts.map((post) => (
+            <ListItem key={post.id}>{post.title}</ListItem>
+          ))}
+        </UnorderedList>
+      </Flex>
+      <Flex flexDirection="column">
+        <Heading>Posts from Context</Heading>
+        <UnorderedList>
+          {state.map((post, index) => (
+            <ListItem key={index}>{post.title}</ListItem>
+          ))}
+        </UnorderedList>
+      </Flex>
+    </Flex>
   );
 }
